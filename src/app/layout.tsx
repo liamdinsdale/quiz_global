d9217c9c2a6c9558a412f0b159ef2e7e27badd07
liamdinsdale <151539231+liamdinsdale@users.noreply.github.com>
2023-11-22 10:52:39 +0000

    initial push

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+import './globals.css'
+
+const roboto = Roboto({
+  weight: "300",
+  subsets: ["latin"]
+});
+
+export const metadata: Metadata = {
+  title: 'Quiz App',
+  description: 'Quiz App for Global Information',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  )
+}
